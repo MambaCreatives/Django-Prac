1947d8166c809762b6d82995cff7278a5d2d3e28
MambaCreatives <alvinkiptoo26@gmail.com>
2024-11-12 13:46:55 +0300

    Created a superuser

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%@(rl4#c)5zsx-aynvfmh$@qbc!1_0%^6kx$khjs6+_x5r*_it'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
