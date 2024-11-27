01ac63822e238c407e1915b42c0991acb67e4618
MambaCreatives <alvinkiptoo26@gmail.com>
2024-11-27 23:52:05 +0300

    testing

--- members/views.py
+++ members/views.py
@@ -22,7 +22,7 @@ def main(request):
   return HttpResponse(template.render())
 
 def testing(request):
-  mydata = Member.objects.filter(firstname='Emil').values()
+  mydata = Member.objects.all().order_by('lastname', '-id').values()
   template = loader.get_template('template.html')
   context ={
     'mymembers' :mydata,
